fb39287a8bfaf6628d99d75f84b3c3371117a22e
Jeff Weiss <jweiss@redhat.com>
2010-06-09 22:04:08 -0400

    added lein file

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject test-clj "0.0.1"
+	  :description "A testNG like test harness for clojure"
+	  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+			 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+	  :dev-dependencies [[swank-clojure "1.2.1"]]
+	  :repositories {"clojure-releases" "http://build.clojure.org/releases"})
