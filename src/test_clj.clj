8df6a7351e70c40147ce4cb301e519cabf318052
Jeff Weiss <jweiss@redhat.com>
2010-06-16 21:32:04 -0400

    improving code and also breaking tpreviously working functionality.

--- src/test_clj.clj
+++ src/test_clj.clj
@@ -7,7 +7,7 @@
 ;------------------------------
 
 (defn #^{:test {:groups #{:group1 :group2} 
-                :dependsOnTest #'test2}}
+                :dependsOnTests #'test2}}
   test1	[]
   (do (println "running test1")
       (println "test1 complete")))
@@ -18,18 +18,19 @@
      (println "test2 complete")))
 	
 (defn #^{:test {:groups #{:group2 :group3}
-		:dependsOnTest #'test2}}
+		:dependsOnTests #'test2}}
   test3	[]
   (do (println "running test3")
       (throw (RuntimeException. "test failed!"))
       (println "test3 complete")))
 
 (defn #^{:test {:groups #{:group1 :group3}
-		:dependsOnTest #'test3}}
+		:dependsOnTests #'test3}}
   test4	[]
   (do (println "running test4")
       ;;throw (RuntimeException. "test failed!"))
       (println "test4 complete")))
+;;(defn #^{:beforeGroups []  })
 
 ;end sample tests
 ;--------------------------------
@@ -39,7 +40,7 @@ either :pass, :skip, or an exception, conj'd onto the end"
   [test prev-results]
   (let [dep (dependency test)
 	result (if (and (not= dep nil) 
-			(not= (dep prev-results) :pass))
+			(not= (prev-results dep) :pass))
 		 :skip
 		 (try (test) 
 		      :pass
@@ -51,19 +52,28 @@ out any tests that shouldn't be run.  Returns a map of test fn's to their result
   ([]
      (run-tests-matching test?))
   ([testfilter]
-     (let [tests (sorted-tests (filter 
-				testfilter 
-				(vals (ns-publics *ns*))))]
+     (let [tests (sorted-tests 
+		  (filter testfilter (vals (ns-publics *ns*))))]
        (loop [remaining-tests tests
-	      results []]
+	      results {}]
 	 (let [test (first remaining-tests)]
-	   (if (empty? remaining-tests) results
-	       (recur 
-		(rest remaining-tests) 
-		(conj results {test (execute-test test results)}))))))))
+	   (if (empty? remaining-tests) 
+	     results
+	     (recur 
+	      (rest remaining-tests) 
+	      (execute-test test results))))))))
 
 (defn dependency [test]
-  (-> (meta test) :test :dependsOnTest))
+  (let [dep-annotation (-> (meta test) :test :dependsOnTests)]
+    (cond (or (nil? dep-annotation) 
+	      (and (set? dep-annotation)
+		   (every? var? dep-annotation)))     
+	  dep-annotation
+	  
+	  (var? dep-annotation) #{dep-annotation}
+	  
+	  true (throw (IllegalArgumentException. ":dependsOnTests must contain either a set of vars or one var")))))
+
 
 (defn in-group? [group myfn]
   (contains? 
@@ -73,20 +83,26 @@ out any tests that shouldn't be run.  Returns a map of test fn's to their result
 (defn test? [myfn]
   (contains? (meta myfn) :test))
 	
-(defn compare-tests-order [arg1 arg2]
+(defn compare-tests-order [test1 test2]
   ;;first check args are right type
-  (if (not (and (var? arg1) 
-		(var? arg2))) 
-    (throw (IllegalArgumentException. (format "Both arguments should be a var. Got: %s, %s" arg1 arg2))))
-  (let [dep1 (dependency arg1)
-	dep2 (dependency arg2)]
-    (if (and (not (nil? dep1)) 
-	     (= dep1 arg2)) ;run arg1 before arg2
+  (if (not (and (var? test1) 
+		(var? test2))) 
+    (throw (IllegalArgumentException. (format "Both arguments should be a var. Got: %s, %s" test1 test2))))
+  (let [deps1 (dependency test1)
+	deps2 (dependency test2)]
+    (if (and (not (nil? deps1)) 
+	     (contains? deps1 test2)) ;run test2 before test1
       1
-      (if (and (not (nil? dep2)) 
-	       (= dep2 arg1)) ;run arg2 before arg1
+      (if (and (not (nil? deps2)) 
+	       (contains? deps2 test1)) ;run test1 before test2
 	-1
 	0)))) ;don't care what order
 
 (defn sorted-tests [tests]
   (sort compare-tests-order tests))
+
+;;hmm...why didn't it run test1
+;;test-clj> (run-tests-matching)
+;;running test2
+;;test2 complete
+;;{#'test-clj/test4 :skip, #'test-clj/test3 :skip, #'test-clj/test1 :skip, #'test-clj/test2 :pass}
