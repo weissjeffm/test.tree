a6816e695693efca95eae25110aaa6e572c87f51
Jeff Weiss <jweiss@redhat.com>
2011-06-14 16:13:31 -0400

    trashing the place, hope to put together again

--- src/test_clj/meta.clj
+++ src/test_clj/meta.clj
@@ -1,27 +1,12 @@
 (ns test-clj.meta)
 ;;deals with test metadata structures
 
-(def config-map 
-     {:before-suite    -3
-      :before-group    -2
-      :before-test     -1
-      nil              0
-      :after-test      1
-      :after-group     2
-      :after-suite     3})
+
 
 (defn get-1stlvl-tag [test tag]
   (-> (meta test) :test tag))
 
-(defn dependencies [test]
-  (let [dep (get-1stlvl-tag test :dependsOnTests)]
-    (cond (nil? dep)  (hash-set)
-          (set? dep)  dep
-	  (var? dep)  (hash-set dep)
-	  true        (throw (RuntimeException.
-			      (format "Test %s has a dependency that isn't a var
-                                       reference to another test function, nor a set
-                                       of such functions." (:name (meta test))))))))
+
 
 (defn configuration [test]
   (get-1stlvl-tag test :configuration))
@@ -38,15 +23,13 @@
 	  (str (:ns (meta test)))
 	  (:name (meta test))))
 
-(defn in-group? [group myfn]
-  (contains? 
-   (->(meta myfn) :test :groups) 
-   group))
+(defn in-group? [group mytest]
+  (some #{group}
+   (mytest :groups)))
 
 (defn in-groups? [groups myfn]
   (some #(in-group? % myfn) groups))
 
-(defn test? [myfn]
-  (contains? (meta myfn) :test))
+
 
 
--- src/test_clj/results.clj
+++ src/test_clj/results.clj
@@ -1,10 +1,7 @@
 (ns test-clj.results
   (:require [test-clj.meta :as meta]))
 
-(defn dependencies "Get the results set for all the dependencies of a test"
-  [results test]
-  (let [test-deps (meta/dependencies test)]
-    (filter #(contains? test-deps (:fn %)) results)))
+
 
 (defn lookup [results test]
   (filter #(= (:fn %) test) results))
