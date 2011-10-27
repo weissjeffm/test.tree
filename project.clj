980e53ae7a54d8b3fe2f63c6af6401bfc2b56e57
Jeff Weiss <jweiss@redhat.com>
2011-10-27 13:37:51 -0400

    limit jvm size

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
 	  :dependencies [[org.clojure/clojure "1.3.0"]
                          [org.clojure/clojure-contrib "1.2.0"]
                          [pretzel "0.2.2"]
-                         [clj-stacktrace "0.2.3"]])
+                         [clj-stacktrace "0.2.3"]]
+          :jvm-opts ["-Xmx256m"])
