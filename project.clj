7de8813d38204b4b371ccaa9f621ee3f4e4874f8
Jeff Weiss <jweiss@redhat.com>
2012-01-05 15:41:07 -0200

    Update stacktrace lib to match swank-clojure

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
                          [org.clojure/core.incubator "0.1.0"]
                          [weissjeffm/clojure.prxml "1.3.0-SNAPSHOT"]
                          [org.clojars.weissjeffm/serializable-fn "1.2.1-SNAPSHOT"]
-                         [clj-stacktrace "0.2.3"]])
+                         [clj-stacktrace "0.2.4"]])
