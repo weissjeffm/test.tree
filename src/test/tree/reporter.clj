327d11c6d2050b25c97d269202e141011d2a197e
Jeff Weiss <jweiss@redhat.com>
2012-01-19 23:28:35 -0200

    added testng xml report

--- src/test/tree/reporter.clj
+++ src/test/tree/reporter.clj
@@ -24,17 +24,23 @@
 (defn configuration? [test]
   (boolean (:configuration test)))
 
+(defn skipped? [test]
+  (= (result test) :skip))
+
 (defn skipped-tests []
   (filter (fn [t] (and (not (configuration? t))
-                       (= (result t) :skip))) (keys @reports)))
+                      (skipped? t)))
+          (keys @reports)))
 
 (defn passed-tests []
   (filter (fn [t] (and (not (configuration? t))
                             (= (result t) :pass))) (keys @reports)))
 
+(defn failed? [t]
+  (isa? (class (result t)) Throwable))
+
 (defn failed-tests []
-  (filter (fn [n] 
-            (isa? (class (result n)) Throwable)) (keys @reports)))
+  (filter failed? (keys @reports)))
 
 (defn execution-time [test]
   (let [r (report test)
@@ -93,3 +99,53 @@
                                            {})]]))
                           (for [pass passes]
                             [:testcase (info pass)]))]))))
+
+(defn testng-report "Produce an xml report consistent with the
+                    testng report schema.  Tries to be especially
+                    compatible with Jenkins."
+  []
+  (let [fails (failed-tests)
+        skips (skipped-tests)
+        passes (passed-tests)
+        [numfail numskip numpass] (map count [fails skips passes])
+        total (+ numfail numskip numpass)
+        grouped-by-class (group-by :name (keys @reports))
+        to-ms-str #(-> (* % 1000) Math/round str)
+        suite-duration-ms (to-ms-str (total-time))
+        info (fn [t] {:name (:name t)
+                     :is-config (-> t :configuration boolean str)
+                     :duration-ms (to-ms-str (execution-time t))
+                     :status (cond (skipped? t) "SKIP"
+                                   (passed? t) "PASS"
+                                   (failed? t) "FAIL")
+                     :signature (try (format "%s%s" (:name t) (-> t :steps second))
+                                     (catch Exception e "sig"))})]
+    (with-out-str
+      (xml/prxml [:decl! {:version "1.0"} ]
+                 [:testng-results {:total (str total)
+                                   :failed (str numfail)
+                                   :passed (str numpass)
+                                   :skipped (str numskip)}
+                  [:reporter-output] ;;empty
+                  [:suite {:name "Test Suite"
+                           :duration-ms suite-duration-ms}
+                   [:test {:name "Test Tree"
+                           :duration-ms suite-duration-ms}
+                    (for [[class methods] grouped-by-class]
+                      [:class {:name class}
+                       (for [method methods]
+                         (let [tr (@reports method)]
+                           [:test-method (info method)
+                            (when (skipped? method)
+                              [:exception {:class "Skipped"}
+                               [:message [:cdata! (format "Blocked by: %s"
+                                                          (pr-str (get-in tr [:report :blocked-by])))]]])
+                            (when (failed? method)
+                              (let [e (result method)]
+                                [:exception {:class (-> e .getClass str)}
+                                 [:message [:cdata! (.getMessage e)]]
+                                 [:full-stacktrace [:cdata! (pst-str e)]]]))
+                            (if-let [params (:parameters method)] 
+                              [:params (map (fn [i p] [:param {:index i}
+                                                      [:value [:cdata! p]]])
+                                            (iterate inc 0) params)])]))])]]]))))
