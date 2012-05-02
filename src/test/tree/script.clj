e5aa1b3dda3b810acac817899879680da1ae09b6
Jeff Weiss <jweiss@redhat.com>
2012-05-02 18:36:44 -0400

    rework some of the macros, still need a macro to clean up those repeated ns imports.

--- src/test/tree/script.clj
+++ src/test/tree/script.clj
@@ -1,6 +1,5 @@
 (ns test.tree.script
-  (:use [test.tree.builder :only [data-driven]])
-  (:refer-clojure :exclude [test]))
+  (:use [test.tree.builder :only [data-driven]]))
 
 (defn split-opts [args]
   "split a list at the first item that's in an even index (zero based)
@@ -16,26 +15,25 @@
     (split-with #(not= (resolve (first %)) deftest-var) args)))
 
 (defmacro deftest [testname & options+steps]
-  (let [[options steps] (split-opts options+steps)
-        [steps dependent-tests] (split-deftests steps)]
-    (merge `{:name ~testname
-             :steps (serializable.fn/fn [] ~@steps)}
-           (apply hash-map options)
-           (if (not (empty? dependent-tests))
-             {:more (vec dependent-tests)} {}))))
-
-(defmacro defgroup [ & forms]
-  `(do (ns ~(symbol (first forms))
-         (:use test.tree.builder
-               test.tree.script))
-       ~@(rest forms)))
-
-(defmacro data [argnames & rows]
-  (vec rows))
+  (if (= testname :data-driven)
+    (conj options+steps `deftest-datadriven)
+    (let [[options steps] (split-opts options+steps)
+          [steps dependent-tests] (split-deftests steps)]
+      (merge `{:name ~testname
+               :steps (serializable.fn/fn [] ~@steps)}
+             (apply hash-map options)
+             (if (not (empty? dependent-tests))
+               {:more (vec dependent-tests)} {})))))
 
 (defmacro deftest-datadriven [testname & options+kw+data]
-  (let [[options [kw & data]] (split-opts options+kw+data)
+  (let [[options [kw & rows]] (split-opts options+kw+data)
         thistest (merge {:name testname
-                               :steps kw}
+                         :steps kw}
                         (apply hash-map options))]
-    `(data-driven ~thistest ~(vec data))))
+    `(data-driven ~thistest ~@rows)))
+
+;; (defmacro defgroup [ & forms]
+;;   `(do (ns ~(symbol (first forms))
+;;          (:use test.tree.builder
+;;                test.tree.script))
+;;        ~@(rest forms)))
