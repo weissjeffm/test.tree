281335ccccf7cbafadb079f224d48af51d296de1
Jeff Weiss <jweiss@redhat.com>
2012-07-09 12:14:12 -0400

    don't skip a minor version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject test.tree "0.9-SNAPSHOT"
+(defproject test.tree "0.8-SNAPSHOT"
 	  :description "A testNG like test harness for clojure"
 	  :dependencies [[org.clojure/clojure "1.3.0"]
                          [org.clojure/core.incubator "0.1.0"]
