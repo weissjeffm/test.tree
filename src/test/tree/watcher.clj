ac2c2176d4a37a0b0b3f0216418b3e7f5c6acbd2
Jeff Weiss <jweiss@redhat.com>
2013-02-23 17:27:55 -0500

    Fix watchers to handle the change to where report data is stored in a promise.

--- src/test/tree/watcher.clj
+++ src/test/tree/watcher.clj
@@ -2,12 +2,12 @@
   (:use [clojure.data :as data]
         [test.tree.reporter :only [configuration?]]))
 
-(defn log-watcher [_ _ old new]
-  (println "Received event! " (second (data/diff old new))))
+(defn log-watcher [_ _ old new_]
+  (println "Received event! " (second (data/diff old new_))))
 
 (defn watch-on-pred [pred f]
-  (fn [_ _ old new]
-    (let [[_ added _] (data/diff old new)]
+  (fn [_ _ old new_]
+    (let [[_ added _] (data/diff old new_)]
       (doseq [[test report] added]
         (when (pred test report)
           (f test report))))))
@@ -16,33 +16,38 @@
   "create a watcher that will call f when a test fails."
   [f]
   (watch-on-pred (fn [test report]
-                   (and (-> report :report :result (= :fail))
-                        (not (configuration? test))))
+                   (let [r (:report report)]
+                     (and (realized? r)
+                          (-> r deref :result (= :fail))
+                          (not (configuration? test)))))
                  f))
 
 (defn status-watcher
   "create a watcher that will call f when a test's :status equals
    status."
   [status f]
-  (fn [_ _ old new]
-    (let [[_  added _] (data/diff old new)]
+  (fn [_ _ old new_]
+    (let [[_  added _] (data/diff old new_)]
       (doseq [[test report] added]
         (when (= (:status report) status)
           (f test report))))))
 
 (defn stdout-log-watcher
   "Prints test summaries to stdout as tests are completed."
-  [k r old new]
-  (let [[_ added _] (data/diff old new)]
-    (doseq [[{:keys [name parameters]} {:keys [status report]}] added]
-      (let [parms-str (if parameters (pr-str parameters) "")
-            blocked-by (:blocked-by report)
-            bb-str (if blocked-by (str "Blockers: " (pr-str blocked-by)) "")
-            exception (or (-> report :error :throwable) "")]
-        (if (= status :done)
-          (println (apply format "%-12s %s %s   %s%s"
-                          (map str [(:result report)
-                                    name
-                                    parms-str
-                                    bb-str
-                                    (.toString exception)]))))))))
+  [k r old new_]
+  (let [[_ added _] (data/diff old new_)]
+    
+    (doseq [[{:keys [name parameters] :as t} {:keys [status]}] added]
+      (let [report (-> t new_ :report)
+            parms-str (if parameters (pr-str parameters) "")]
+        (if (realized? report)
+          (let [report @report
+                blocked-by (:blocked-by report)
+                bb-str (if blocked-by (str "Blockers: " (pr-str blocked-by)) "")
+                exception (or (-> report :error :throwable) "")]
+            (println (apply format "%-12s %s %s   %s%s"
+                            (map str [(:result report)
+                                      name
+                                      parms-str
+                                      bb-str
+                                      (.toString exception)])))))))))
