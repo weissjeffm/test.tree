4bfce7bc5d371690985d067c6085aaaf01edb3dc
Jeff Weiss <jweiss@redhat.com>
2013-06-20 10:25:18 -0400

    Use patched version of data.xml

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.incubator "0.1.0"]
                  [hickory "0.4.1"]
-                 [org.clojure/data.xml "0.0.7"]
+                 ;; [org.clojure/data.xml "0.0.7"] ;; waiting for bugfix DXML-17
+                 [weissjeffm/data.xml "0.0.8-SNAPSHOT"]
                  [org.clojars.weissjeffm/serializable-fn "1.2.1-SNAPSHOT"]
                  [clj-stacktrace "0.2.5"]
                  [slingshot "0.10.1"]]
