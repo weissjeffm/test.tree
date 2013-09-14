8c5ab1afeb56389a12beac67a17ea5d597ada678
Jeff Weiss <jweiss@redhat.com>
2013-09-14 15:23:59 -0400

    Experimental work on graph traversing test execution

--- src/secant_test/core.clj
+++ src/secant_test/core.clj
@@ -1,9 +1,7 @@
 (ns secant-test.core
-  (:require [loom.graph :as graph]
-            [loom.attr :as attr]
-            loom.io
-            [loom.alg :as graph-alg]
-            [slingshot.slingshot :refer [try+]])
+  (:require [slingshot.slingshot :refer [try+]]
+            [loom.graph :as graph]
+            [loom.alg :as graph-alg])
   (:import [java.util.concurrent LinkedBlockingQueue])
   (:refer-clojure :exclude [partial]))
 
@@ -15,17 +13,32 @@
   (error-check [o] "Check a procedure for obvious errors.")
   (partial [o args] "Partial application of arguments to a function."))
 
+(defprotocol Passable
+  (outcome [o] "Returns :pass :fail or :skip"))
+
 (defrecord Test [name description steps parameters]
   Testable
-  (as-tests [o] (list o))
+  (as-tests [t] (list t))
 
   Invokable
-  (invoke [this]
-    (invoke (:steps this)))
-  (error-check [this]
-    (error-check (partial (:steps this)
-                         (or (:parameters this) (vector)))))
-  (partial [o args] (assoc o :parameters args)))
+  (invoke [t]
+    (invoke (:steps t)))
+  (error-check [t]
+    (error-check (partial (:steps t)
+                         (or (:parameters t) (vector)))))
+  (partial [t args] (assoc t :parameters args))) ;; TODO not sure if this is right
+
+(declare pass?)
+
+(defrecord TestResult [test]
+  Passable
+  (outcome [t]
+    (or (:error t)
+        (and (:verifications t)
+             (->> t :verifications (every? pass?)))))) ;; FIXME
+
+(defn pass? [o]
+  (= (outcome o) :pass))
 
 (defrecord Verification [expr target-str pass?])
 
@@ -43,59 +56,79 @@
                                     :partial clojure.core/partial 
                                     :error-check (constantly nil)}) ; functions are already compiled, no further checking to do
 
+(defn mktest [{:keys [name description steps parameters]}]
+  (let [t (Test. name description steps parameters)]
+    (error-check t)
+    t))
+
+(defmacro deftest
+  "Define a test given a map, and put in a var with the given symbol."
+  [sym m]
+  `(def  ~(with-meta sym {:dynamic true}) (mktest ~m)))
+
+;; Printing
+
+;; Print clojure records with #Foopkg.fooclass{:a "bar"} style
+(defmethod clojure.pprint/simple-dispatch clojure.lang.IRecord [obj] (pr obj))
+(prefer-method clojure.pprint/simple-dispatch clojure.lang.IRecord clojure.lang.IPersistentMap)
+
+
 
 ;; assertions
-(def ^{:dynamic true, :doc "var to hold assertion results, do not use directly"}
-  test-assertions nil)
+(def ^{:dynamic true,
+       :doc "var to hold assertion results, do not use directly"}
+  test-verifications (atom (vector)))
 
-(defmacro assert* [x message]
+(defmacro verify [x message]
   `(let [v# (Verification. (quote ~x) ~message false)
          v#  (try+
               (assert ~x ~message)
               (assoc v# :pass? true)
               (catch Object o# (assoc v# :error o#)) )]
-     (swap! test-assertions conj v#)
+     (swap! test-verifications conj v#)
      v#))
-
-(defmacro with-assertions
-  "Collect assertions (called with assert), and return them as a list at the end of body"
-  [& body]
-  `(binding [test-assertions (atom (vector))]
-     ~@body
-     test-assertions))
-
-
-(defmacro defmytest [sym name & body]
-  `(deftest ~sym {:name ~name :steps (quote (with-assertions ~@body))}))
 
 ;; test runner wrappings
 
 (defn run-test [{:keys [test]}]
   (invoke test))
 
-(defn wrap-assertions [runner]
-  (fn [req]
-    (with-assertions 
-      (runner req))))
+(defn wrap-watch "Allows test execution to be watched" [runner]
+  (fn [{:keys [test]}]
+    ()))
+(defn wrap-dependencies
+  "Skips tests when their dependencies are not met"
+  [runner]
+  (fn [{:keys [test dependencies]}]
+    (if (some ))))
 
-(defn wrap-failures [runner]
-  (fn [req]
-    (try+
-     {:returned (run-test (:test req))
-      :outcome :pass}
-     (catch Object _ {:outcome :fail
-                      :error &throw-context}))))
+(defn wrap-verifications [runner]
+  (fn [{:keys [verifications :as req]}]
+    (binding [test-verifications (or verifications
+                                     (atom (vector)))] 
+      (assoc (runner req) :verifications (deref test-verifications)))))
+
+(defn wrap-errors [runner]
+  (fn [{:keys [test] :as req}]
+    (let [run-res (try+
+                   (runner req)
+                   (catch Object _ {:error &throw-context}))]
+      (assoc run-res :test test))))
 
 (def default-runner
   (-> run-test
-      wrap-failures))
+      wrap-errors
+      wrap-verifications))
 
-(defn consume [runner q]
-  (loop [t (.poll q)]
-    (and t (do (runner {:test t})
-               (recur (.poll q))))))
+(defn- consume
+  [{:keys [runner test-graph-ref queue]}]
+  (loop [t (.poll queue)]
+    (and t
+         (do (dosync (commute output-ref conj (runner {:test t
+                                                       :dependencies (graph/incoming t)})))
+             (recur (.poll queue))))))
 
-(defn start-testrun [tests & [{:keys [thread-wrapper threads
+(defn start-testrun [test-graph & [{:keys [thread-wrapper threads
                                       watchers reports-ref
                                       runner]
                                :or {thread-wrapper identity
@@ -103,16 +136,24 @@
                                     runner default-runner
                                     watchers {}}}]]
   (let [testrun-queue (LinkedBlockingQueue.)
+        output-ref (or reports-ref (ref (vector)))
+        traversal (graph-alg/bf-traverse test-graph)
         thread-pool (for [thread-num (range threads)]
-                      (Thread. (-> (partial consume testrun-queue)
+                      (Thread. (-> consume
+                                   (clojure.core/partial {:runner default-runner
+                                                          :queue testrun-queue
+                                                          :output-ref output-ref
+                                                          :test-graph test-graph})
                                    thread-wrapper)
                                (str "secant_test-thread" thread-num)))]
     ;;queue all tests
-    (doseq [t tests]
+    (doseq [t traversal]
       (.put testrun-queue t))
 
     ;;start worker threads
-    (doseq [thread thread-pool] (.start thread))))
+    
+    (doseq [thread thread-pool]
+      (.start thread))
+    output-ref))
 
-(comment (loom.io/view mytests2 :node-label :name))
-(comment (map :name (graph-alg/bf-traverse mytests2)) )
+
